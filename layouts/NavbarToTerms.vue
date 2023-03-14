5c5798fc724c998d96e3d71b91c7fe08c1fcbb66
Juan Felipe Lopez Estrada <juanfelipepipen@gmail.com>
2023-03-13 20:03:31 -0700

    SPTM x2

--- layouts/NavbarToTerms.vue
+++ layouts/NavbarToTerms.vue
@@ -3,7 +3,7 @@
     <nav class="flex h-24 bg-black">
       <div class="container flex flex-wrap justify-center items-center mx-auto">
         <a href="" class="flex items-center">
-          <img src="assets/logo.png" class="mx-auto mr-3 h-6 sm:h-14" alt="Flowbite Logo"/>
+          <img src="/assets/logo.png" class="mx-auto mr-3 h-6 sm:h-14" alt="Flowbite Logo"/>
         </a>
       </div>
     </nav>
